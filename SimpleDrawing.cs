2a2c3b7ad3ca5ecf5b83a7c0bef7094b2b35ea82
Markus Haslinger <markus@marce.at>
2023-03-05 18:20:12 +0100

    initial

---
+++ SimpleDrawing.cs
@@ -0,0 +1,294 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Media;
+using Avalonia.Themes.Fluent;
+
+namespace SimpleXPlatDrawing;
+
+public static class SimpleDrawing
+{
+    private const double MinThickness = 0.1D;
+    private static readonly IBrush defaultBrush = Brushes.Black;
+    private static readonly object mutex;
+    private static readonly List<DrawTask> tasks;
+    private static App? _app;
+    private static bool _initDone;
+
+    static SimpleDrawing()
+    {
+        tasks = new();
+        _app = null;
+        mutex = new object();
+    }
+
+    private static int Width { get; set; }
+    private static int Height { get; set; }
+
+    public static void Init(int width, int height)
+    {
+        if (_initDone)
+        {
+            Console.WriteLine("Duplicate initialization!");
+            return;
+        }
+
+        _initDone = true;
+        Width = width;
+        Height = height;
+    }
+
+    public static bool DrawLine(Point start, Point end, double thickness = 1D, IBrush? color = null)
+    {
+        if (!ValidatePoints(start, end) || thickness < MinThickness)
+        {
+            return false;
+        }
+
+        color ??= defaultBrush;
+
+        AddTask(new LineDrawTask(start, end, thickness, color));
+
+        return true;
+    }
+
+    public static bool DrawRectangle(Point topLeft, Point bottomRight, double lineThickness = 1D,
+                                     IBrush? lineColor = null, IBrush? fillColor = null)
+    {
+        if (!ValidatePoints(topLeft, bottomRight) || lineThickness < MinThickness)
+        {
+            return false;
+        }
+
+        lineColor ??= defaultBrush;
+
+        AddTask(new RectDrawTask(topLeft, bottomRight, lineThickness, lineColor, fillColor));
+
+        return true;
+    }
+
+    public static bool DrawEllipse(Point center, double radiusX, double radiusY, double lineThickness = 1D,
+                                   IBrush? lineColor = null, IBrush? fillColor = null)
+    {
+        Point[] corners = new[]
+        {
+            new Point(center.X + radiusX / 2D, center.Y),
+            new Point(center.X - radiusX / 2D, center.Y),
+            new Point(center.X, center.Y + radiusY / 2D),
+            new Point(center.X, center.Y - radiusY / 2D)
+        };
+        if (radiusX < MinThickness || radiusY < MinThickness
+                                   || !ValidatePoints(corners.Concat(new[] { center }))
+                                   || lineThickness < MinThickness)
+        {
+            return false;
+        }
+
+        lineColor ??= defaultBrush;
+
+        AddTask(new EllipseDrawTask(center, radiusX, radiusY, lineThickness, lineColor, fillColor));
+
+        return true;
+    }
+
+    public static bool DrawCircle(Point center, double radius, double lineThickness = 1D,
+                                  IBrush? lineColor = null, IBrush? fillColor = null) =>
+        DrawEllipse(center, radius, radius, lineThickness, lineColor, fillColor);
+
+    public static bool DrawText(Point origin, string text, double fontSize, IBrush? textColor = null)
+    {
+        if (!ValidatePoint(origin) || fontSize < MinThickness)
+        {
+            return false;
+        }
+
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            // invisible, don't draw at all
+            return true;
+        }
+        
+        textColor ??= defaultBrush;
+        
+        AddTask(new TextDrawTask(origin, text, fontSize, textColor));
+
+        return true;
+    }
+
+    public static void Clear()
+    {
+        lock (mutex)
+        {
+            tasks.Clear();
+        }
+    }
+
+    private static void AddTask(DrawTask task)
+    {
+        lock (mutex)
+        {
+            tasks.Add(task);
+        }
+    }
+
+    public static async Task Render()
+    {
+        if (_app == null)
+        {
+            var appBuilder = AppBuilder.Configure<App>().UsePlatformDetect();
+            #pragma warning disable CS4014
+            Task.Run(() =>
+            {
+                appBuilder.StartWithClassicDesktopLifetime(Array.Empty<string>());
+            });
+            #pragma warning restore CS4014
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            _app = appBuilder.Instance as App;
+        }
+        
+        _app?.Refresh();
+    }
+
+    private static bool ValidatePoints(params Point[] points) => ValidatePoints(points.AsEnumerable());
+    private static bool ValidatePoints(IEnumerable<Point> points) => points.All(ValidatePoint);
+
+    private static bool ValidatePoint(Point point) =>
+        point is { X: >= 0, Y: >= 0 }
+        && point.X <= Width && point.Y <= Height;
+
+    private abstract class DrawTask
+    {
+        protected DrawTask(double thickness, IBrush color)
+        {
+            Pen = new Pen(color, thickness, lineCap: PenLineCap.Round);
+        }
+
+        protected Pen Pen { get; }
+
+        public abstract void DrawSelf(DrawingContext context);
+    }
+
+    private sealed class LineDrawTask : DrawTask
+    {
+        private readonly Point _end;
+        private readonly Point _start;
+
+        public LineDrawTask(Point start, Point end, double thickness, IBrush color) : base(thickness, color)
+        {
+            _start = start;
+            _end = end;
+        }
+
+        public override void DrawSelf(DrawingContext context)
+        {
+            context.DrawLine(Pen, _start, _end);
+        }
+    }
+
+    private sealed class RectDrawTask : DrawTask
+    {
+        private readonly Point _bottomRight;
+        private readonly IBrush? _fillColor;
+        private readonly Point _topLeft;
+
+        public RectDrawTask(Point topLeft, Point bottomRight, double thickness, IBrush lineColor, IBrush? fillColor)
+            : base(thickness, lineColor)
+        {
+            _topLeft = topLeft;
+            _bottomRight = bottomRight;
+            _fillColor = fillColor;
+        }
+
+        public override void DrawSelf(DrawingContext context)
+        {
+            context.DrawRectangle(_fillColor, Pen, new(_topLeft, _bottomRight));
+        }
+    }
+
+    private sealed class EllipseDrawTask : DrawTask
+    {
+        private readonly Point _center;
+        private readonly IBrush? _fillColor;
+        private readonly double _radiusX;
+        private readonly double _radiusY;
+
+        public EllipseDrawTask(Point center, double radiusX, double radiusY, double lineThickness,
+                               IBrush lineColor, IBrush? fillColor) : base(lineThickness, lineColor)
+        {
+            _center = center;
+            _radiusX = radiusX;
+            _radiusY = radiusY;
+            _fillColor = fillColor;
+        }
+
+        public override void DrawSelf(DrawingContext context)
+        {
+            context.DrawEllipse(_fillColor, Pen, _center, _radiusX, _radiusY);
+        }
+    }
+
+    private sealed class TextDrawTask : DrawTask
+    {
+        private readonly Point _origin;
+        private readonly FormattedText _text;
+
+        public TextDrawTask(Point origin, string text, double emSize, IBrush textColor) : base(0, defaultBrush)
+        {
+            _origin = origin;
+            _text = new(text, CultureInfo.InvariantCulture, FlowDirection.LeftToRight,
+                        Typeface.Default, emSize, textColor);
+        }
+
+        public override void DrawSelf(DrawingContext context)
+        {
+            context.DrawText(_text, _origin);
+        }
+    }
+
+    private sealed class App : Application
+    {
+        private Window _mainWindow = default!;
+        private Canvas? _canvas;
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            Styles.Add(new FluentTheme());
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                _canvas = new Canvas();
+                _mainWindow = new Window
+                {
+                    Title = "SimpleDrawing",
+                    Width = Width + 5,
+                    Height = Height + 5,
+                    Content = _canvas
+                };
+                desktop.MainWindow = _mainWindow;
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+        public void Refresh()
+        {
+            _canvas?.InvalidateVisual();
+        }
+
+        private sealed class Canvas : Control
+        {
+            public override void Render(DrawingContext context)
+            {
+                DrawTask[] renderTasks;
+                lock (mutex)
+                {
+                    renderTasks = tasks.ToArray();
+                }
+                foreach (var drawTask in renderTasks)
+                {
+                    drawTask.DrawSelf(context);
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+# SimpleDrawing - Cross Platform
+
+- Allows for the simple drawing of basic shapes on a canvas
+- Renders the canvas in a window
+- Works on multiple platforms (tested Windows, Linux, Mac)
+- Based on SkiaSharp
+  - via Avalonia UI 
+
+## Sample Usage
+
+```csharp
+using Avalonia.Media;
+using SimpleXPlatDrawing;
+
+SimpleDrawing.Init(400, 400);
+
+SimpleDrawing.DrawLine(new(100, 100), new(300, 300));
+SimpleDrawing.DrawLine(new(300, 100), new(100, 300), thickness: 4, color: Brushes.Green);
+SimpleDrawing.DrawRectangle(new(50, 50), new(100, 80), lineColor: Brushes.Red);
+SimpleDrawing.DrawRectangle(new(325, 325), new(375, 375), fillColor: Brushes.Azure);
+SimpleDrawing.DrawEllipse(new(200, 300), 50, 75, 
+                          lineColor: Brushes.Firebrick, fillColor: Brushes.Firebrick);
+SimpleDrawing.DrawCircle(new(350, 75), 40, lineThickness: 2.5D, 
+                         lineColor: Brushes.BlueViolet, fillColor: Brushes.Gold);
+SimpleDrawing.DrawText(new(25, 350), "Hello World", 24, Brushes.Lime);
+
+await SimpleDrawing.Render();
+
+SimpleDrawing.DrawLine(new(200, 25), new(200, 375), 8);
+await SimpleDrawing.Render();
+
+await Task.Delay(TimeSpan.FromSeconds(2));
+SimpleDrawing.Clear();
+var x = 50;
+for (var i = 0; i < 10; i++)
+{
+    SimpleDrawing.DrawLine(new(x, 25), new(x, 375), 8);
+    x += 25;
+    await SimpleDrawing.Render();
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+Console.Write("Press any key to exit...");
+Console.ReadKey();
+```
